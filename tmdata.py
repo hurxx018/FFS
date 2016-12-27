bef9cfa0ed68a6d0841af3d37ebe06ff6c2718b9
Kwang Ho Hur <hurxx018@gmail.com>
2016-12-27 15:53:23 -0600

    tmdata was updated

--- tmdata.py
+++ tmdata.py
@@ -31,11 +31,27 @@ class tmdata(object):
             raise TypeError("frequency is not an integer.")
         self._data = [None]*len(channels)
 
+    def rebin(self, cha=1, lbin=1):
+        self._checkchalbin(cha, lbin)
+        if lbin == 1:
+            return self._data[cha-1], self._frequency
+        else:
+            bindatasize = self._data[cha-1].size//lbin
+            k = (self._data[cha-1][:bindatasize*lbin]
+                     .reshape(bindatasize, lbin).sum(axis=1))
+            return k, self._frequency/lbin
+
     def _checkdata(self):
         if self.nchannels > 1:
             for i in range(self.nchannels - 1):
                 assert len(self._data[i]) == len(self._data[i+1]), \
                     "The number of data points does not match."
+
+    def _checkchalbin(self, cha, lbin):
+        if not (isinstance(cha, int) and (cha in range(1, self.nchannels+1))):
+            raise ValueError("{} is not available".format(cha))
+        if not isinstance(lbin, int):
+            raise TypeError('The type of lbin is int.')
 
     @property
     def filenames(self):
@@ -86,3 +102,94 @@ class tmdata(object):
     @property
     def tsampling(self):
         return 1./self._frequency
+
+
+class tmdataSR(tmdata):
+    """tmdata with the memoization of rebinned data
+
+    For a given cha and lbin, perform the rebin individually .
+    """
+
+    def __init__(self, filenames=[], channels=[], frequency=1):
+        super(tmdataRebin, self).__init__(filenames, channels, frequency)
+        self._rebindata = {}
+        self._rebindata[1] = self.data
+        self._rebinfreq = {}
+        self._rebinfreq[1] = self.frequency
+
+    def rebin(self, cha=1, lbin=1):
+        self._checkchalbin(cha, lbin)
+        try:
+            if self._rebindata[lbin][cha-1] is not None:
+                return self._rebindata[lbin][cha-1], self._rebinfreq[lbin]
+            else:
+                k, f = self._rebin(cha, lbin)
+                self._rebindata[lbin][cha-1] = k
+                return k, f
+        except:
+            self._rebindata.setdefault(lbin, [None]*self.nchannels)
+            k, f = self._rebin(cha, lbin)
+            self._rebindata[lbin][cha-1] = k
+            self._rebinfreq[lbin] = f
+            return k, f
+
+    def _rebin(self, cha, lbin):
+        for i in sorted(self._rebindata.keys(), reverse=True):
+            if lbin % i == 0 and (self._rebindata[i][cha-1] is not None):
+                temp_ds = self._rebindata[i][cha-1].size
+                temp_lbin = lbin // i
+                bds = temp_ds // (temp_lbin)
+                k = (self._rebindata[i][cha-1][:bds*temp_lbin]
+                            .reshape(bds, temp_lbin).sum(axis=1))
+                f = self._rebinfreq[i] / temp_lbin
+                return k, f
+
+    @property
+    def rebindata(self):
+        return self._rebindata
+    @property
+    def rebinfrequency(self):
+        return self._rebinfrequency
+
+
+class tmdataMR(tmdata):
+    """tmdata with the memoization of rebinned data
+
+    For a given rebin-factor, this rebins data of all channels and memorizes
+    them.
+    """
+
+    def __init__(self, filenames=[], channels=[], frequency=1):
+        super(tmdataMR, self).__init__(filenames, channels, frequency)
+        self._rebindata = {}
+        self._rebindata[1] = self.data
+        self._rebinfreq = {}
+        self._rebinfreq[1] = self.frequency
+
+    def rebin(self, cha=1, lbin=1):
+        self._checkchalbin(cha, lbin)
+        try:
+            return self._rebindata[lbin][cha-1], self._rebinfreq[lbin]
+        except:
+            k, f = self._rebin(cha, lbin)
+            self._rebindata[lbin] = k
+            self._rebinfreq[lbin] = f
+            return k[cha-1], f
+
+    def _rebin(self, cha, lbin):
+        for i in sorted(self._rebindata.keys(), reverse=True):
+            if lbin % i == 0:
+                temp_ds = self._rebindata[i][0].size
+                temp_lbin = lbin // i
+                bds = temp_ds // (temp_lbin)
+                k = [temp_k[:bds*temp_lbin].reshape(bds, temp_lbin).sum(axis=1)
+                        for temp_k in self._rebindata[i]]
+                f = self._rebinfreq[i] / temp_lbin
+                return k, f
+
+    @property
+    def rebindata(self):
+        return self._rebindata
+    @property
+    def rebinfrequency(self):
+        return self._rebinfrequency
