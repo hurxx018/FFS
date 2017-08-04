f2314829abc764ca7aac0c38f09888703d485d6e
Kwang Ho Hur <hurxx018@gmail.com>
2017-08-04 15:28:15 -0500

    Updata MSQTransformer.py and rFFS.py

--- MSQTransformer.py
+++ MSQTransformer.py
@@ -4,6 +4,136 @@ import numpy as np
 from tmdata import tmdata
 from FFSTransformer import FFSTransformer
 
+def dark(X_dark, binfactors, segmentlength=32768*4, channel=1, timeshift=1):
+    """ Calculate k1 and k2 from tmdata in terms of binfactors
+
+    X_dark >> tmdata for dark counts
+    binfactors >> list
+    segmentlength >> default 32768*4
+    channel >>
+    timeshift >>  a factor of timeshift; an integer
+
+    ...
+
+    """
+    k1 = np.zeros( len(binfactors) )
+    k2 = np.zeros( len(binfactors) )
+    data = X_dark.ch(channel, 1)
+    for i, b in enumerate(binfactors):
+        nsegs = data.size//segmentlength*b
+        temp = data[:nsegs*segmentlength//b].reshape(nsegs, segmentlength//b)
+        if timeshift > 0:
+            t_k1_1 = data[:, :-timeshift].mean(axis=1)
+            t_k1_2 = data[:, timeshift:].mean(axis=1)
+            t_k2 = (data[:, :-timeshift]*data[:, timeshift:]).mean(axis=1) - t_k1_1*t_k1_2
+            k1[i] = (t_k1_1 + t_k1_2).mean(axis=0)/2.
+            k2[i] = t_k2.mean(axis=0)
+        elif timeshift == 0:
+            t_k1 = data.mean(axis=1)
+            t_k2 = np.power(data, 2).mean(axis=1) - t_k1 - np.power(t_k1, 2)
+            k1[i] = t_k1.mean(axis=0)
+            k2[i] = t_k2.mean(axis=0)
+        else:
+            raise ValueError("timeshift is an integer of 0 or positive.")
+    return (k1, k2)
+
+
+def msq_est_dark(X, binfactors, segmentlength=32768*4, channel=1, timeshift=1, dark=None):
+    """ Calculate MSQ estimators with dark count corrections
+
+    X_dark >> tmdata for dark counts
+    binfactors >> list
+    segmentlength >> default 32768*4
+    channel >>
+    timeshift >>  a factor of timeshift; an integer
+
+    dark >> [k1, k2] from a function dark
+
+    """
+    if dark:
+        assert len(binfactors) == dark[0].size, "inconsistency in the size of dark k1 and k2"
+    else:
+        raise ValueError("dark is not available.")
+
+    msq = np.zeros( len(binfactors) )
+    msq_err = np.zeros( len(binfactors) )
+
+    data = X.ch(channel, 1)
+    for i, b in enumerate(binfactors):
+        nsegs = data.size//segmentlength*b
+
+        dark1 = np.ones(nsegs) * dark[0][i]
+        dark2 = np.ones(nsegs) * dark[1][i]
+
+        temp = data[:nsegs*segmentlength//b].reshape(nsegs, segmentlength//b)
+        if timeshift > 0:
+            t_k1_1 = data[:, :-timeshift].mean(axis=1)
+            t_k1_2 = data[:, timeshift:].mean(axis=1)
+            t_k2 = (data[:, :-timeshift]*data[:, timeshift:]).mean(axis=1) - t_k1_1*t_k1_2
+
+            m1 = (t_k1_1 + t_k1_2)/2. - dark1   # dark count correction
+            m2 = t_k2 - dark2                   # dark count correction
+            q = m2/m1
+            msq[i] = q.mean()
+            msq_err[i] = q.std()
+
+        elif timeshift == 0:
+            t_k1 = data.mean(axis=1)
+            t_k2 = np.power(data, 2).mean(axis=1) - t_k1 - np.power(t_k1, 2)
+
+            m1 = t_k1 - dark1
+            m2 = t_k2 - dark2
+            q = m2/m1
+            msq[i] = q.mean()
+            msq_err[i] = q.std()
+
+        else:
+            raise ValueError("timeshift is an integer of 0 or positive.")
+    return (msq, msq_err)
+
+
+def msq_est(X, binfactors, segmentlength=32768*4, channel=1, timeshift=1):
+    """Calculate MSQ estimators without dark count corrections
+
+    X_dark >> tmdata for dark counts
+    binfactors >> list
+    segmentlength >> default 32768*4
+    channel >>
+    timeshift >>  a factor of timeshift; an integer
+
+    """
+
+    msq = np.zeros( len(binfactors) )
+    msq_err = np.zeros( len(binfactors) )
+
+    data = X.ch(channel, 1)
+    for i, b in enumerate(binfactors):
+        nsegs = data.size//segmentlength*b
+
+        temp = data[:nsegs*segmentlength//b].reshape(nsegs, segmentlength//b)
+        if timeshift > 0:
+            t_k1_1 = data[:, :-timeshift].mean(axis=1)
+            t_k1_2 = data[:, timeshift:].mean(axis=1)
+            t_k2 = (data[:, :-timeshift]*data[:, timeshift:]).mean(axis=1) - t_k1_1*t_k1_2
+
+            q = t_k2/(t_k1_1 + t_k1_2)*2.
+            msq[i] = q.mean()
+            msq_err[i] = q.std()
+
+        elif timeshift == 0:
+            t_k1 = data.mean(axis=1)
+            t_k2 = np.power(data, 2).mean(axis=1) - t_k1 - np.power(t_k1, 2)
+
+            q = t_k2/t_k1
+            msq[i] = q.mean()
+            msq_err[i] = q.std()
+
+        else:
+            raise ValueError("timeshift is an integer of 0 or positive.")
+    return (msq, msq_err)
+
+
+
 class MSQTransformer(FFSTransformer):
     """Calculate mean of segmented Q values
 
@@ -29,92 +159,112 @@ class MSQTransformer(FFSTransformer):
             raise TypeError("tshift should be an integer value.")
         self._tshift = tshift
 
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
+    # def fit(self, X, y=None):
+    #     return self
+    def dark(self, X):
         if not isinstance(X, tmdata):
-            raise TypeError()
-        if self._channels == []:
-            data = X.data
-            channels = X.channels
-            frequency = X.frequency
-            result = self.calqestimators(data, channels, frequency)
+            raise TypeError("X for dark counts should be a type of tmdata.")
+        return dark(X, self.binfactors, self.segmentlength, self.channels[0], self.timeshift)
+
+
+    def transform(self, X, dark=None):
+        """ Calculate MSQ estimators where dark counts can be taken into accout
+
+        dark >> [k1, k2] from dark counts
+            (k1 - the first cumulants, k2 - the second cumulants , where each lenghth
+            should be the same to the lenghth of binfactors)
+
+        """
+        if not isinstance(X, tmdata):
+            raise TypeError("X for counts should be a type of tmdata.")
+        if dark:
+            return msq_est_dark(X, self.binfactors, self.segmentlength, self.channels[0], self.timeshift, dark)
         else:
-            data = X.data
-            channels = X.channels
-            frequency = X.frequency
-            result = self.calqestimators(data, self._channels, frequency)
-        return result
-
-    def calqestimators(self, data, channels, frequency):
-        result = {}
-        for i in channels:
-            if self._tshift == 0:
-                result[i] = self.sub_qestimators(data[i - 1], frequency)
-            else:
-                result[i] = self.sub_qestimators_withtshift(data[i - 1], \
-                                                    frequency)
-        return result
-
-    def sub_qestimators(self, array, frequency):
-        """Calculate qestimators for tshift == 0 where the shot noise term is
-        considered.
+            return msq_est(X, self.binfactors, self.segmentlength, self.channels[0], self.timeshift)
 
 
-        """
-        time = np.zeros(self._binfactors.size)
-        qestimator = np.zeros(self._binfactors.size)
-        qestimator_stderr = np.zeros(self._binfactors.size)
-        for i, binf in enumerate(self._binfactors):
-            time[i] = (self._segmentlength // binf)/float(frequency)
+        # if not isinstance(X, tmdata):
+        #     raise TypeError()
+        # if self._channels == []:
+        #     data = X.data
+        #     channels = X.channels
+        #     frequency = X.frequency
+        #     result = self.calqestimators(X, channels, frequency)
+        # else:
+        #     data = X.data
+        #     channels = X.channels
+        #     frequency = X.frequency
+        #     result = self.calqestimators(X, self._channels, frequency)
+        # return result
 
-            n_segments = array.size // self._segmentlength * binf
-            temp_data = array[0:n_segments*(self._segmentlength//binf)] \
-                        .reshape((n_segments, self._segmentlength//binf))
-            temp_k1 = temp_data.mean(axis=1)
-            temp_k2 = (temp_data*temp_data).mean(axis=1)
-            temp_q = temp_k2/temp_k1 - temp_k1 - 1.
-            qestimator[i] = temp_q.mean()
-            if n_segments > 1:
-                qestimator_stderr[i] = temp_q.std()/np.sqrt(n_segments - 1.)
-            else:
-                qestimator_stderr[i] = np.abs(qestimator[i]) * 0.1 #TO DO
-
-        qestimator_tuple = collections.namedtuple("Qestimator", \
-                    ["time", "qestimator", "qestimator_stderr", "tsfhit"])
-        return qestimator_tuple(time, qestimator, qestimator_stderr, self._tshift)
-
-    def sub_qestimators_withtshift(self, array, frequency):
-        """Calculate qestimators for tshift >= 1 where no shot noise term exist.
-
-        """
-        time = np.zeros(self._binfactors.size)
-        qestimator = np.zeros(self._binfactors.size)
-        qestimator_stderr = np.zeros(self._binfactors.size)
-        index = 0
-        for i, binf in enumerate(self._binfactors):
-            time[i] = (self._segmentlength // binf)/float(frequency)
-
-            n_segments = array.size // self._segmentlength * binf
-            temp_data = (array[0:n_segments*(self._segmentlength//binf)]
-                        .reshape((n_segments, self._segmentlength//binf)))
-
-            mean1 = temp_data[:, :-self._tshift].mean(axis=1).reshape((-1, 1))
-            mean2 = temp_data[:, self._tshift:].mean(axis=1).reshape((-1, 1))
-            temp_q = (((temp_data[:, :-self._tshift] - mean1)
-                        *(temp_data[:, self._tshift:] - mean2)).mean(axis=1)
-                        .reshape((-1, 1))
-                        /(mean1 + mean2) * 2 )
-            qestimator[i] = temp_q.mean()
-            if n_segments > 1:
-                qestimator_stderr[i] = temp_q.std()/np.sqrt(n_segments)
-            else:
-                qestimator_stderr[i] = np.abs(qestimator[i]) * 0.1
-
-        qestimator_tuple = collections.namedtuple("Qestimator", \
-                    ["time", "qestimator", "qestimator_stderr", "tshift"])
-        return qestimator_tuple(time, qestimator, qestimator_stderr, self._tshift)
+    # def calqestimators(self, data, channels, frequency):
+    #     result = {}
+    #     for i in channels:
+    #         if self._tshift == 0:
+    #             result[i] = self.sub_qestimators(data[i - 1], frequency)
+    #         else:
+    #             result[i] = self.sub_qestimators_withtshift(data[i - 1], \
+    #                                                 frequency)
+    #     return result
+    #
+    # def sub_qestimators(self, array, frequency):
+    #     """Calculate qestimators for tshift == 0 where the shot noise term is
+    #     considered.
+    #
+    #
+    #     """
+    #     time = np.zeros(self._binfactors.size)
+    #     qestimator = np.zeros(self._binfactors.size)
+    #     qestimator_stderr = np.zeros(self._binfactors.size)
+    #     for i, binf in enumerate(self._binfactors):
+    #         time[i] = (self._segmentlength // binf)/float(frequency)
+    #
+    #         n_segments = array.size // self._segmentlength * binf
+    #         temp_data = array[0:n_segments*(self._segmentlength//binf)] \
+    #                     .reshape((n_segments, self._segmentlength//binf))
+    #         temp_k1 = temp_data.mean(axis=1)
+    #         temp_k2 = (temp_data*temp_data).mean(axis=1)
+    #         temp_q = temp_k2/temp_k1 - temp_k1 - 1.
+    #         qestimator[i] = temp_q.mean()
+    #         if n_segments > 1:
+    #             qestimator_stderr[i] = temp_q.std()/np.sqrt(n_segments - 1.)
+    #         else:
+    #             qestimator_stderr[i] = np.abs(qestimator[i]) * 0.1 #TO DO
+    #
+    #     qestimator_tuple = collections.namedtuple("Qestimator", \
+    #                 ["time", "qestimator", "qestimator_stderr", "tsfhit"])
+    #     return qestimator_tuple(time, qestimator, qestimator_stderr, self._tshift)
+    #
+    # def sub_qestimators_withtshift(self, array, frequency):
+    #     """Calculate qestimators for tshift >= 1 where no shot noise term exist.
+    #
+    #     """
+    #     time = np.zeros(self._binfactors.size)
+    #     qestimator = np.zeros(self._binfactors.size)
+    #     qestimator_stderr = np.zeros(self._binfactors.size)
+    #     index = 0
+    #     for i, binf in enumerate(self._binfactors):
+    #         time[i] = (self._segmentlength // binf)/float(frequency)
+    #
+    #         n_segments = array.size // self._segmentlength * binf
+    #         temp_data = (array[0:n_segments*(self._segmentlength//binf)]
+    #                     .reshape((n_segments, self._segmentlength//binf)))
+    #
+    #         mean1 = temp_data[:, :-self._tshift].mean(axis=1).reshape((-1, 1))
+    #         mean2 = temp_data[:, self._tshift:].mean(axis=1).reshape((-1, 1))
+    #         temp_q = (((temp_data[:, :-self._tshift] - mean1)
+    #                     *(temp_data[:, self._tshift:] - mean2)).mean(axis=1)
+    #                     .reshape((-1, 1))
+    #                     /(mean1 + mean2) * 2 )
+    #         qestimator[i] = temp_q.mean()
+    #         if n_segments > 1:
+    #             qestimator_stderr[i] = temp_q.std()/np.sqrt(n_segments)
+    #         else:
+    #             qestimator_stderr[i] = np.abs(qestimator[i]) * 0.1
+    #
+    #     qestimator_tuple = collections.namedtuple("Qestimator", \
+    #                 ["time", "qestimator", "qestimator_stderr", "tshift"])
+    #     return qestimator_tuple(time, qestimator, qestimator_stderr, self._tshift)
 
 
     @property
--- rFFS.py
+++ rFFS.py
@@ -10,7 +10,7 @@ def iss0(filenames, nch):
     return data
 
 def flex8(filenames, nch):
-    with open(self.filenames[0]) as f:
+    with open(filenames[0]) as f:
         temp_data = np.fromfile(f, dtype=np.uint8)
     if nch == 1:
         return [temp_data]
@@ -21,7 +21,7 @@ def flex8(filenames, nch):
         raise ValueError("channels are incorrectly assigned.")
 
 def flex16(filenames, nch):
-    with open(self.filenames[0]) as f:
+    with open(filenames[0]) as f:
         temp_data = np.fromfile(f, dtype=np.uint16)
     if nch == 1:
         return [temp_data]
@@ -32,7 +32,7 @@ def flex16(filenames, nch):
         raise ValueError("channels are incorrectly assigned.")
 
 
-class rFFS(tmdataSR):
+class rFFS(object):
     """
     Read FFS data from bin files (###.bin) which are collected at timemode
     input variables:
@@ -46,55 +46,68 @@ class rFFS(tmdataSR):
     instance methods:
     readFFSData : read a data for each filename (32bit integer)
     """
-    daqcards = {"iss0":iss0, "flex8":flex8, "flex16":flex16}
+    daqcards = {"iss0":iss0, "flex8":flex8, "flex16":flex16, "sim":0}
 
-    def __init__(self, filenames=[], channels=[], frequency=1, daqcard=None):
-        super(rFFS, self).__init__(filenames, channels, frequency)
-        #tmdata.__init__(self, filenames, channels, frequency)
+    def __init__(self, channels=[], frequency=1, daqcard=None):
         if daqcard not in self.daqcards.keys():
             print("daqcard can be either one among")
             for key in self.daqcards.keys():
                 print(key)
             raise ValueError("daqcard is not given correctly.")
         self._daqcard = daqcard
-        self._data = self.readFFSData()
-        self._checkdata()
-        self.assign_rebindata()
+        self._channels = channels
+        self._frequency = frequency
 
-    def readFFSData(self):
-        if not self.filenames:
-            return []
-        else:
-            return self.daqcards[self._daqcard](self.filenames, self.nchannels)
+    def load(self, filenames=[]):
+        tm_dataSR = tmdataSR(filenames, self._channels, self._frequency)
+        if filenames:
+            if self._daqcard != "sim":
+                tm_dataSR.data = self.daqcards[self._daqcard](filenames, tm_dataSR.nchannels)
+                tm_dataSR._checkdata()
+        tm_dataSR.assign_rebindata()
+        return tm_dataSR
 
-    def get_tmdataMR(self):
-        temp = tmdatMR(self.filenames, self.channels, self.frequency)
-        temp.data = self.data
-        temp.rebinfrequency = self.rebinfrequency
-        temp.rebindata = self.rebindata
-        return temp
+    # def get_tmdataMR(self, tm_data):     This can be deprecatted for the easy management
+    #     # get a tmdataMR for the convenience of rebinning
+    #     if not isinstance(tm_data, tmdata):
+    #         raise TypeError("Type of input is not tmdata.")
+    #     tm_dataMR = tmdataMR(tm_data.filenames, tm_data.channels, tm_data.frequency)
+    #     tm_dataMR.data = tm_data.data
+    #     tm_dataMR.assign_rebindata()
+    #     return tm_dataMR
 
-    @property
-    def filenames(self):
-        return self._filenames
+def main():
 
-    @filenames.setter
-    def filenames(self, filenames):
-        if isinstance(filenames, list):
-            self._filenames = filenames
-            self._data = self.readFFSData()
-            self._checkdata()
-        else:
-            raise TypeError("filenames is not a python list.")
+    # Use the iss cards
+    filename1 = "A488_cal.1.001.bin"
+    filename2 = "A488_cal.2.001.bin"
+    filename3 = "A488_cal.3.001.bin"
+    ffsdata = rFFS( [1, 2, 3], 100000, "iss0" )
+    temp = ffsdata.load( [filename1, filename2, filename3] )
+    print(temp)
+    print("data :")
+    for data in temp.data:
+        print(data[0:20])
+    print("")
 
-    def info(self):
-        """
-        Returns the information in the class
-        """
-        return  {key:value for key, value in self.__dict__.items()
-                         if not key.startswith('__') and not callable(key)}
+    # Use the Flex card
+    filename1 = "alexa_05.dat"
+    ffsdata = rFFS([1, 2], 100000, "flex8") # 8bit
+    temp = ffsdata.load([filename1])
+    print(temp)
+    print("data :")
+    for data in temp.data:
+        print(data[0:20])
+    print()
 
-    def __str__(self):
-        for key, value in self.info().items():
-            print("{0}  :   {1}".format(key, value))
-        return ""
+    # For the simulation
+    ffsdata = rFFS([1, 2], 100000, "sim")
+    temp = ffsdata.load([])
+    temp.data = [np.ones(32768*2, dtype=int), np.ones(32768*2, dtype=int)]
+    temp.assign_rebindata()
+    print(temp)
+    for data in temp.data:
+        print(data[0:20])
+
+if __name__ == '__main__':
+    main()
